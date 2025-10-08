87bce022f01eb73dbceeaa235b68f2ad53dfa89b
Arnav Jain <jainarnav107@gmail.com>
2025-10-08 20:25:33 +0530

    Create config.py

---
+++ firmware/modules/config.py
@@ -0,0 +1,15 @@
+# All pins in one place
+PINS = {
+    "OLED_SDA": 0, "OLED_SCL": 1,
+    "SERVO": 2,
+    "RAIN": 14, "IR": 15, "DHT": 16,
+    "RELAY1": 17, "RELAY2": 18, "BUZZER": 19,
+    "TRIG": 20, "ECHO": 21,
+    "LED": 22, "LDR_ADC": 26
+}
+
+TIMING = {
+    "pump_on_s": 2.0,
+    "beep_short_ms": 120,
+    "beep_long_ms": 400
+}
